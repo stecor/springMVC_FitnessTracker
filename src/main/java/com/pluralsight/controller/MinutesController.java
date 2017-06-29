933e7ee753c3bedeb3860e0db362e56b217bb3c4
Stefano Corra <stefano.corra@hotmail.com>
2017-06-28 21:13:26 -0400

    third commit

---
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -0,0 +1,13 @@
+package com.pluralsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MinutesController {
+
+	@RequestMapping(value = "/addMinutes")
+	public String addMinutes() {
+		return "addMinutes";
+	}
+}
